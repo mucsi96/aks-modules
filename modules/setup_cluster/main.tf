389b3b2c1ce223e9293c0c7e9c59acc49806589f
Igor Bari <mucsi96@gmail.com>
2024-05-26 21:03:56 +0200

    provision cluster

--- modules/setup_cluster/main.tf
+++ modules/setup_cluster/main.tf
@@ -11,3 +11,43 @@ resource "azurerm_resource_group" "resource_group" {
   name     = var.azure_resource_group_name
   location = var.azure_location
 }
+
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = azurerm_resource_group.resource_group.name
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = azurerm_resource_group.resource_group.location
+  address_space       = ["10.0.0.0/12"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = azurerm_resource_group.resource_group.name
+  resource_group_name  = azurerm_resource_group.resource_group.name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
+  address_prefixes     = ["10.1.0.0/16"]
+}
+
+resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
+  name                              = azurerm_resource_group.resource_group.name
+  location                          = azurerm_resource_group.resource_group.location
+  resource_group_name               = azurerm_resource_group.resource_group.name
+  dns_prefix                        = azurerm_resource_group.resource_group.name
+  role_based_access_control_enabled = true
+  kubernetes_version                = var.azure_k8s_version
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 1
+    vm_size         = var.azure_vm_size
+    os_disk_size_gb = var.azure_vm_disk_size_gb
+    vnet_subnet_id  = azurerm_subnet.subnet.id
+  }
+
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "basic"
+  }
+}
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 brew update && brew install azure-cli terraform
+
+terraform init
