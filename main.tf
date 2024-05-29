14509e16a9cc034e48fbd8a40e4a909a9f1138c0
Igor Bari <mucsi96@gmail.com>
2024-05-29 21:40:31 +0200

    add dns setup

--- main.tf
+++ main.tf
@@ -14,22 +14,29 @@ output "k8s_admin_config" {
   sensitive = true
 }
 
-# data "azurerm_key_vault_secret" "dns_zone" {
-#   name = local.azure_resource_group_name
-#   key_vault_id  = "dns-zone"
-# }
+data "azurerm_key_vault" "kv" {
+  resource_group_name = local.azure_resource_group_name
+  name                = local.azure_resource_group_name
+}
 
-# data "azurerm_key_vault_secret" "ip_range" {
-#   name = local.azure_resource_group_name
-#   key_vault_id  = "ip-range"
-# }
+data "azurerm_key_vault_secret" "dns_zone" {
+  key_vault_id = data.azurerm_key_vault.kv.id
+  name         = "dns-zone"
+}
 
-# module "setup_ingress_controller" {
-#   source                    = "./modules/setup_ingress_controller"
-#   azure_resource_group_name = local.azure_resource_group_name
-#   azure_location            = local.azure_location
-#   dns_zone                  = data.azurerm_key_vault_secret.dns_zone.value
-#   k8s_admin_config_path     = module.setup_cluster.k8s_admin_config_path
-#   traefik_chart_version     = local.traefik_chart_version
-#   ip_range                  = data.azurerm_key_vault_secret.ip_range.value
-# }
+data "azurerm_key_vault_secret" "ip_range" {
+  key_vault_id = data.azurerm_key_vault.kv.id
+  name         = "ip-range"
+}
+
+module "setup_ingress_controller" {
+  depends_on = [module.setup_cluster]
+
+  source                    = "./modules/setup_ingress_controller"
+  azure_resource_group_name = local.azure_resource_group_name
+  azure_location            = local.azure_location
+  dns_zone                  = data.azurerm_key_vault_secret.dns_zone.value
+  k8s_admin_config_path     = ".kube/admin-config"
+  traefik_chart_version     = local.traefik_chart_version
+  ip_range                  = data.azurerm_key_vault_secret.ip_range.value
+}
--- modules/setup_ingress_controller/main.tf
+++ modules/setup_ingress_controller/main.tf
@@ -11,3 +11,16 @@ resource "azurerm_dns_zone" "dns_zone" {
   resource_group_name = var.azure_resource_group_name
   name                = var.dns_zone
 }
+
+data "azurerm_public_ip" "public_ip" {
+  resource_group_name = var.azure_resource_group_name
+  name                = var.azure_resource_group_name
+}
+
+resource "azurerm_dns_a_record" "dns_a_record" {
+  resource_group_name = var.azure_resource_group_name
+  zone_name           = azurerm_dns_zone.dns_zone.name
+  name                = "*"
+  ttl                 = 300
+  records             = [data.azurerm_public_ip.public_ip.ip_address]
+}
