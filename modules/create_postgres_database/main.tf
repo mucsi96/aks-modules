f8c74c9970e0f6dd9c7a749618268b11fe0c8fa3
Igor Bari <mucsi96@gmail.com>
2024-07-31 19:45:18 +0200

    add db

--- modules/create_postgres_database/main.tf
+++ modules/create_postgres_database/main.tf
@@ -2,6 +2,7 @@ resource "random_string" "db_username" {
   length  = 12
   upper   = false
   numeric = false
+  special = false
 }
 
 resource "random_password" "db_password" {
@@ -12,25 +13,26 @@ resource "random_string" "exporter_username" {
   length  = 12
   upper   = false
   numeric = false
+  special = false
 }
 
 resource "random_password" "exporter_password" {
   length = 20
 }
 
-# resource "helm_release" "database" {
-#   name       = var.k8s_name
-#   repository = "https://mucsi96.github.io/k8s-helm-charts"
-#   chart      = "postgres-db"
-#   version    = "1.0.0"
-#   namespace  = var.k8s_namespace
-#   wait       = true
-#   # https://github.com/mucsi96/k8s-helm-charts/tree/main/charts/postgres_db
-#   values = [yamlencode({
-#     name             = var.db_name
-#     username         = random_string.db_username.result
-#     password         = random_password.db_password.result
-#     exporterUsername = random_string.exporter_username.result
-#     exporterPassword = random_password.exporter_password.result
-#   })]
-# }
+resource "helm_release" "database" {
+  name       = var.k8s_name
+  repository = "https://mucsi96.github.io/k8s-helm-charts"
+  chart      = "postgres-db"
+  version    = "1.0.0"
+  namespace  = var.k8s_namespace
+  wait       = true
+  # https://github.com/mucsi96/k8s-helm-charts/tree/main/charts/postgres_db
+  values = [yamlencode({
+    name             = var.db_name
+    username         = random_string.db_username.result
+    password         = random_password.db_password.result
+    exporterUsername = random_string.exporter_username.result
+    exporterPassword = random_password.exporter_password.result
+  })]
+}
