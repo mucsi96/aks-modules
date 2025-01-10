60598fded84f1637cc75679b0930dd38713d0eef
Igor Bari <mucsi96@gmail.com>
2025-01-10 08:17:05 +0100

    add Log Analytics workspace and solution for Kubernetes cluster monitoring

--- modules/setup_cluster/main.tf
+++ modules/setup_cluster/main.tf
@@ -23,6 +23,26 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.1.0.0/16"]
 }
 
+resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
+  name                = "ibari-${var.azure_resource_group_name}"
+  location            = var.azure_location
+  resource_group_name = var.azure_resource_group_name
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_log_analytics_solution" "log_analytics_solution" {
+  solution_name         = "ContainerInsights"
+  location              = var.azure_location
+  resource_group_name   = var.azure_resource_group_name
+  workspace_resource_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
+  workspace_name        = azurerm_log_analytics_workspace.log_analytics_workspace.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
+}
+
 resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
   name                              = var.azure_resource_group_name
   location                          = var.azure_location
@@ -59,6 +79,10 @@ resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
   network_profile {
     network_plugin    = "kubenet"
     load_balancer_sku = "standard"
+  }
+
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
   }
 }
 
