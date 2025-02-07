3c3092161d96505d36fd9bf5d2030fea81266093
Igor Bari <mucsi96@gmail.com>
2025-02-07 08:08:53 +0100

    feat: update role assignments for Backup and Learn Language APIs; add new storage containers and permissions

--- modules/setup_backup_app/main.tf
+++ modules/setup_backup_app/main.tf
@@ -1,8 +1,3 @@
-data "azurerm_storage_account" "storage_account" {
-  name                = var.azure_storage_account_name
-  resource_group_name = var.azure_storage_account_resource_group_name
-}
-
 module "create_backup_namespace" {
   source                    = "../create_app_namespace"
   azure_resource_group_name = var.azure_resource_group_name
@@ -32,10 +27,4 @@ module "setup_backup_spa" {
   api_id        = module.setup_backup_api.application_id
   api_client_id = module.setup_backup_api.client_id
   api_scope_ids = module.setup_backup_api.scope_ids
-}
-
-resource "azurerm_role_assignment" "allow_backup_api_to_write_storage_account" {
-  scope                = data.azurerm_storage_account.storage_account.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = module.setup_backup_api.resource_object_id
 }
--- permissions.tf
+++ permissions.tf
@@ -3,14 +3,31 @@ data "azurerm_storage_account" "storage_account" {
   resource_group_name = "ibari"
 }
 
-data "azurerm_storage_container" "storage_container" {
+data "azurerm_storage_container" "learn_language_storage_container" {
   name               = "learn-language"
+  storage_account_id = data.azurerm_storage_account.storage_account.id
+}
+
+data "azurerm_storage_container" "backups_storage_container" {
+  name               = "backups"
   storage_account_id = data.azurerm_storage_account.storage_account.id
 }
 
 /**
  * Backup App
  */
+resource "azurerm_role_assignment" "allow_backup_api_to_create_user_delegation_keys" {
+  scope                = data.azurerm_storage_account.storage_account.id
+  role_definition_name = "Storage Blob Delegator"
+  principal_id         = module.setup_backup_app.backup_api_resource_object_id
+}
+
+resource "azurerm_role_assignment" "allow_backup_api_to_write_storage_container" {
+  scope                = data.azurerm_storage_container.backups_storage_container.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = module.setup_backup_app.backup_api_resource_object_id
+}
+
 resource "azuread_app_role_assignment" "allow_admin_user_read_backups" {
   app_role_id         = module.setup_backup_app.backup_api_roles_ids["DatabaseBackupsReader"]
   principal_object_id = module.setup_cluster.owner
@@ -45,8 +62,14 @@ resource "azuread_app_role_assignment" "allow_admin_user_download_backups" {
  * Learn Language
  */
 
+resource "azurerm_role_assignment" "allow_learn_language_api_to_create_user_delegation_keys" {
+  scope                = data.azurerm_storage_account.storage_account.id
+  role_definition_name = "Storage Blob Delegator"
+  principal_id         = module.setup_learn_language_api.resource_object_id
+}
+
 resource "azurerm_role_assignment" "allow_learn_language_api_to_read_and_write_learn_language_storage_container" {
-  scope                = data.azurerm_storage_container.storage_container.id
+  scope                = data.azurerm_storage_container.learn_language_storage_container.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = module.setup_learn_language_api.resource_object_id
 }
